b8b0e124c298ccdcb195d1a66b4387d5b3414225
Ankit Singh <singhankit121.as@gmail.com>
2026-02-26 22:23:37 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,72 @@
+# Gender -> 1 Female 0 Male
+# Churn  -> 1 Yes 0 No
+# Scaler is exported as scaler.pkl
+# Model is exported as model.pkl
+# Order of the X -> 'A', 'Gender', 'Tenure', 'MonthlyCharges'
+
+
+import streamlit as st
+import joblib
+
+import numpy as np
+
+import os
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+model = joblib.load("model.pkl")
+scaler = joblib.load("scaler.pkl")
+
+
+
+st.title("Churn Prediction App")
+st.divider()
+
+st.write("Please enter the values and hit the predict button for getting a prediction.")
+
+st.divider()
+
+age = st.number_input("Enter age", min_value=10, max_value=100, value=30)
+
+
+tenure = st.number_input("Enter Tenure", min_value=0, max_value= 130, value = 10)
+monthlycharge = st.number_input("Enter Monthly Charge", min_value=30, max_value=150)
+
+gender = st.selectbox("Enter the Gender",["Male","Female"])
+
+st.divider()
+
+predictbutton = st.button("Predict")
+
+
+
+
+
+
+
+
+st.divider()
+
+
+if predictbutton:
+    gender_selected = 1 if gender =="Female" else 0
+
+    X = [age, gender_selected, tenure, monthlycharge]
+
+    X1 = np.array(X)
+
+    # X_array = np.array(X).reshape(1, -1)
+
+    X_array = scaler.transform([X1])
+
+    #Scale
+    # X_scaled = scaler.transform(X_array)
+    prediction = model.predict(X_array)[0]
+    
+    predicted = "Yes" if prediction == 1 else "No!" 
+
+    st.balloons()
+
+    st.write(f"Predicted: {predicted}")
+
+else:
+    st.write("Please enter the values and use predict button")
